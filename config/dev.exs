7069492700422c9f05a8627f2804a159375e2a77
zorail <rohan.seth497@gmail.com>
2018-10-22 23:46:18 +0530

    database created

--- config/dev.exs
+++ config/dev.exs
@@ -50,7 +50,7 @@ config :phoenix, :stacktrace_depth, 20
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "rohan@123",
   database: "discuss_dev",
   hostname: "localhost",
   pool_size: 10
