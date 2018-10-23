5d1f6848e6552bf53c97b77ef15c083682660836
zorail <rohan.seth497@gmail.com>
2018-10-24 01:44:23 +0530

    delete cotroller

--- web/router.ex
+++ web/router.ex
@@ -20,7 +20,12 @@ defmodule Discuss.Router do
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
     get "/topics/:id/edit", TopicController, :edit
-    put "topics/:id", TopicController, :update
+    put "/topics/:id", TopicController, :update
+    delete "/topics/:id", TopicController, :delete
+
+    # For restful routes
+    # resources "/", TopicController
+
   end
 
   # Other scopes may use custom stacks.
