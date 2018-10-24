c9fc6775ffb44bcbeee189e5422a58c9d94d303e
zorail <rohan.seth497@gmail.com>
2018-10-25 02:42:48 +0530

    topics associated with comments

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,11 +1,28 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
+  alias Discuss.{Topic, Comment}
+
+
   def join("comments:" <> topic_id, _params, socket) do
-    {:ok, %{hey: "there"}, socket}
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+    {:ok, %{}, assign(socket, :topic, topic)}
   end
 
-  def handle_in(name, message, socket) do
+  def handle_in(name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+
+    changeset = topic
+      |> build_assoc(:comments)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        {:rely, :ok, socket}
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
     {:reply, :ok, socket}
   end
 
