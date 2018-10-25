b7299def5d246b68649cba049590632485c9d75d
zorail <rohan.seth497@gmail.com>
2018-10-25 10:49:14 +0530

    resolved signout bug

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -8,11 +8,11 @@ defmodule Discuss.UserSocket do
   transport :websocket, Phoenix.Transports.WebSocket
 
   def connect(%{"token" => token}, socket) do
-    case Phoenix.Token.verify(socket, "key", token) do
+    case Phoenix.Token.verify(socket, "key", token, max_age: 86400) do
       {:ok, user_id} ->
         {:ok, assign(socket, :user_id, user_id)}
       {:error, _error} ->
-
+        {:error}
     end
     # {:ok, socket}
   end
